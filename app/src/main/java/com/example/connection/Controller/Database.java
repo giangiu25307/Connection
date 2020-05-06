b9e3a30c9b66652c0380acc3539b3e086b1bc615
giangiu25307 <gianlucgiardina@libero.it>
2020-05-06 11:40:06 +0200

    aggiornamento db

--- app/src/main/java/com/example/connection/Controller/Database.java
+++ app/src/main/java/com/example/connection/Controller/Database.java
@@ -6,9 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
-import java.io.File;
 import java.nio.file.Paths;
 
 public class Database extends SQLiteOpenHelper {
@@ -19,69 +16,82 @@ public class Database extends SQLiteOpenHelper {
 
     public Database(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context=context;
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        File f=new File(context.getFilesDir().toString()+"/connection.db");
-        if(f.exists()){
-            db.openOrCreateDatabase(context.getFilesDir().toString()+"/connection.db",null);
-            String query="DELETE FROM IP";
-            db.execSQL(query);
-        }else{
-            db.openOrCreateDatabase(context.getFilesDir().toString()+"/connection.db",null);
-            String CREATE_USER_TABLE = "CREATE TABLE USER ( "
-                    + "id_user INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
-                    + "number INTEGER NOT NULL" + ", "
-                    + "name TEXT NOT NULL" + ", "
-                    + "surname TEXT NOT NULL" + ", "
-                    + "age INTEGER NOT NULL" + ", "
-                    + "gender TEXT NOT NULL" + ", "
-                    + "mail TEXT NOT NULL" + ", "
-                    + "username TEXT NOT NULL" + ", "
-                    + "password TEXT NOT NULL" + ", "
-                    + "country TEXT NOT NULL" + ", "
-                    + "city TEXT NOT NULL"
-                    + ")";
+        String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS " + Task.TaskEntry.USER + " ( "
+                + Task.TaskEntry.ID_USER + " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
+                + Task.TaskEntry.NUMBER + " INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.NAME + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.SURNAME + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.AGE + " INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.GENDER + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.MAIL + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.USERNAME + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.PASSWORD + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.COUNTRY + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.PROFILE_PIC + " TEXT NOT NULL" + ", "
+                + Task.TaskEntry.CITY + " TEXT NOT NULL"
+                + ")";
 
-            String CREATE_MESSAGE_TABLE = "CREATE TABLE MESSAGE ( "
-                    + "id_chat INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
-                    + "msg TEXT" + ", "
-                    + "path TEXT"
-                    + ")";
+        String CREATE_MESSAGE_TABLE = "CREATE TABLE IF NOT EXISTS "+ Task.TaskEntry.MESSAGE +" ( "
+                + Task.TaskEntry.ID_CHAT+ " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
+                + Task.TaskEntry.ID_SENDER+ " INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.MSG+" TEXT" + ", "
+                + Task.TaskEntry.PATH+" TEXT"
+                + ")";
 
-            String CREATE_CHAT_TABLE = "CREATE TABLE CHAT ( "
-                    + "id_chat INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
-                    + "id_sender INTEGER NOT NULL" + ", "
-                    + "id_receiver INTEGER NOT NULL" + ","
-                    + "PRIMARY KEY (id_chat)" + ","
-                    + "FOREIGN KEY (id_chat) REFERENCES MESSAGE (id_chat)"
-                    + ")";
+        String CREATE_GROUP_MESSAGE_TABLE = "CREATE TABLE IF NOT EXISTS "+ Task.TaskEntry.GROUP_MESSAGE +" ( "
+                + Task.TaskEntry.ID_GROUP+ " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
+                + Task.TaskEntry.ID_SENDER+ " INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.MSG+" TEXT" + ", "
+                + Task.TaskEntry.PATH+" TEXT"
+                + ")";
 
-            String CREATE_IP_TABLE = "CREATE TABLE IP ( "
-                    + "id_user INTEGER NOT NULL" + ", "
-                    + "ip TEXT NOT NULL"
-                    + ")";
+        String CREATE_CHAT_TABLE = "CREATE TABLE IF NOT EXISTS "+ Task.TaskEntry.CHAT +" ( "
+                + Task.TaskEntry.ID_CHAT+ " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
+                + Task.TaskEntry.ID_TALKER1+ " INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.ID_TALKER2+ " INTEGER NOT NULL"
+                + ")";
 
-            db.execSQL(CREATE_USER_TABLE);
-            db.execSQL(CREATE_MESSAGE_TABLE);
-            db.execSQL(CREATE_CHAT_TABLE);
-            db.execSQL(CREATE_IP_TABLE);
-        }
+        String CREATE_GROUPS_TABLE = "CREATE TABLE IF NOT EXISTS "+ Task.TaskEntry.GROUPS +" ( "
+                + Task.TaskEntry.ID_GROUP+ " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
+                + Task.TaskEntry.ID_USER+ " INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.IP+ " INTEGER NOT NULL"
+                + ")";
+
+        String CREATE_GLOBAL_MESSAGE_TABLE = "CREATE TABLE IF NOT EXISTS "+ Task.TaskEntry.GLOBAL_MESSAGE +" ( "
+                + Task.TaskEntry.ID_SENDER+ " INTEGER PRIMARY KEY AUTOINCREMENT" + ", "
+                + Task.TaskEntry.MSG+ " INTEGER NOT NULL"
+                + ")";
+
+        String CREATE_IP_TABLE = "CREATE TABLE IF NOT EXISTS "+ Task.TaskEntry.NETWORK_IPS +" ( "
+                + Task.TaskEntry.ID_USER+" INTEGER NOT NULL" + ", "
+                + Task.TaskEntry.IP+ " TEXT"
+                + ")";
+
+        db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(CREATE_MESSAGE_TABLE);
+        db.execSQL(CREATE_CHAT_TABLE);
+        db.execSQL(CREATE_IP_TABLE);
+        db.execSQL(CREATE_GROUPS_TABLE);
+        db.execSQL(CREATE_GROUP_MESSAGE_TABLE);
+        db.execSQL(CREATE_GLOBAL_MESSAGE_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        String DROP_TABLE = "DROP TABLE IF EXISTS USER";
+        /*String DROP_TABLE = "DROP TABLE IF EXISTS USER";
         db.execSQL(DROP_TABLE);
         DROP_TABLE = "DROP TABLE IF EXISTS CHAT";
         db.execSQL(DROP_TABLE);
         DROP_TABLE = "DROP TABLE IF EXISTS MESSAGE";
-        db.execSQL(DROP_TABLE);
+        db.execSQL(DROP_TABLE);*/
         onCreate(db);
 
     }
@@ -118,18 +128,20 @@ public class Database extends SQLiteOpenHelper {
         db.close();
     }
 
-    public String getMsg(int idSender, int idReceiver) {//questo metodo non dovrà ritornare nulla ma mostrare con due appositi metodi o la chat o l'immagine al ricevitore del seguente messaggio
+    public String getMsg(int idTalker1, int idTalker2) {//questo metodo non dovrà ritornare nulla ma mostrare con due appositi metodi o la chat o l'immagine al ricevitore del seguente messaggio
         SQLiteDatabase db = this.getReadableDatabase();
-        String query = "SELECT msg,path" +
-                "FROM CHAT C INNER JOIN MESSAGE M ON M.id_chat = C.id_chat" +
-                "WHERE C.id_sender = '" + idSender + "' and C.id_receiver = '" + idReceiver + "'";
+        String query = "SELECT msg,path,id_sender" +
+                " FROM MESSAGE" +
+                " WHERE id_chat = (SELECT id_chat" +
+                " FROM CHAT" +
+                " WHERE id_talker1='"+idTalker1+"' and id_talker2='"+idTalker2+"' )";
         Cursor c = db.rawQuery(query, null);
         if (c != null) {
             c.moveToFirst();
         }
         db.close();
-        if (!c.getString(1).equals("")) return c.getString(1);//ritorno un messaggio
-        else return c.getString(2);//ritorno una path per esempio per un immagine
+        if (!c.getString(2).equals("")) return c.getString(2);//ritorno un messaggio
+        else return c.getString(3);//ritorno una path per esempio per un immagine
     }
 
 }
--- app/src/main/java/com/example/connection/Controller/Task.java
+++ app/src/main/java/com/example/connection/Controller/Task.java
@@ -5,29 +5,33 @@ import android.provider.BaseColumns;
 public class Task {
 
     public class TaskEntry implements BaseColumns {
-        public static final String USER_TABLE = "USER";
+        public static final String USER = "USER";
         public static final String ID_USER = "id_user";
         public static final String NAME = "name";
         public static final String SURNAME = "surname";
         public static final String AGE = "age";
         public static final String GENDER = "gender";
+        public static final String NUMBER = "number";
         public static final String MAIL = "mail";
         public static final String USERNAME = "username";
         public static final String PASSWORD = "password";
         public static final String COUNTRY = "country";
         public static final String CITY = "city";
         public static final String PROFILE_PIC = "profile_pic";
-        public static final String MESSAGE_TABLE = "MESSAGE";
+        public static final String MESSAGE= "MESSAGE";
         public static final String ID_CHAT = "id_chat";
         public static final String MSG = "msg";
         public static final String PATH = "path";
         public static final String CHAT = "CHAT";
-        public static final String ID_CHAT2 = "id_chat";
+        public static final String ID_TALKER1 = "id_talker1";
         public static final String ID_SENDER = "id_sender";
-        public static final String ID_RECEIVER = "id_receiver";
-        public static final String IP_TABLE = "IP";
-        public static final String ID_USER_FROM_IP = "id_user";
+        public static final String ID_TALKER2 = "id_talker2";
+        public static final String NETWORK_IPS = "NETWORK_IPS";
         public static final String IP = "ip";
+        public static final String GROUPS = "GROUPS";
+        public static final String ID_GROUP = "id_group";
+        public static final String GROUP_MESSAGE = "GROUP_MESSAGE";
+        public static final String GLOBAL_MESSAGE = "GROUPS_MESSAGE";
     }
 
 }
