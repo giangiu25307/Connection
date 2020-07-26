d319baac7812198fcc36ba716e5f1b35490f6d9b
Luca <lucaber.l9@gmail.com>
2020-07-26 16:46:22 +0200

    Creato il dialog per editare le informazioni del profilo, quello per cambiare password, e la voce per recuperare la password nelle impostazioni, più relativo dialog

--- app/src/main/java/com/example/connection/View/SettingsFragment.java
+++ app/src/main/java/com/example/connection/View/SettingsFragment.java
@@ -4,20 +4,19 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -27,9 +26,9 @@ import com.example.connection.Controller.ConnectionController;
 import com.example.connection.Controller.Database;
 import com.example.connection.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
 
-    private ConstraintLayout themeLayout;
+    private ConstraintLayout themeSettings, changePasswordSettings, forgottenPasswordSettings;
     private SharedPreferences sharedPreferences;
     private String newTheme;
     private ConnectionController connectionController;
@@ -37,7 +36,8 @@ public class SettingsFragment extends Fragment {
     private ChatController chatController;
     private int theme = R.style.AppTheme;
     private TextView themeOptionDescription;
-    private int bgColor=R.color.mediumWhite;
+    private Button editProfileButton;
+    private int bgColor = R.color.mediumWhite;
 
     public SettingsFragment() {
 
@@ -70,139 +70,46 @@ public class SettingsFragment extends Fragment {
 
         sharedPreferences = getContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
 
-        themeLayout = view.findViewById(R.id.themeLayout);
-        themeOptionDescription= view.findViewById(R.id.themeOptionDescription);
+        editProfileButton = view.findViewById(R.id.editProfileButton);
+        editProfileButton.setOnClickListener(this);
+
+        themeSettings = view.findViewById(R.id.themeSettings);
+        themeSettings.setOnClickListener(this);
+        themeOptionDescription = view.findViewById(R.id.themeOptionDescription);
         themeOptionDescription.setText(Connection.lightOrDark);
-        themeLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
-                dialogBuilder.setView(R.layout.select_theme_alert_layout);
-                final AlertDialog alertDialog = dialogBuilder.create();
-                alertDialog.show();
 
-                final LinearLayout lightButton, darkButton, followSystemButton;
-                final TextView cancelTextView, applyTextView;
+        changePasswordSettings = view.findViewById(R.id.changePasswordSettings);
+        changePasswordSettings.setOnClickListener(this);
 
-                lightButton = alertDialog.findViewById(R.id.lightButton);
-                darkButton = alertDialog.findViewById(R.id.darkButton);
-                followSystemButton = alertDialog.findViewById(R.id.followSystemButton);
-                cancelTextView = alertDialog.findViewById(R.id.cancelTextView);
-                applyTextView = alertDialog.findViewById(R.id.applyTextView);
-
-
-                String currentTheme = sharedPreferences.getString("appTheme", "light");
-                if(currentTheme.equals("dark")){
-                    theme=R.style.DarkTheme;
-                    newTheme = "dark";
-                    Connection.lightOrDark="Dark";
-                    bgColor=R.color.lightLightBlack;
-                    darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
-                }else if(currentTheme.equals("auto")){
-                    getCurrentSystemTheme();
-                    newTheme = "auto";
-                    Connection.lightOrDark="Follow System";
-                    if(theme==R.style.DarkTheme){
-                        bgColor=R.color.lightLightBlack;
-                    }else{
-                        bgColor=R.color.mediumWhite;
-                    }
-                    followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
-                }else{
-                    theme=R.style.AppTheme;
-                    newTheme = "light";
-                    Connection.lightOrDark="Light";
-                    lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
-                }
-                /*
-                if (currentTheme.equals("light")) {
-                    lightButton.setChecked(true);
-                } else if (currentTheme.equals("dark")) {
-                    darkButton.setChecked(true);
-                } else {
-                    followSystemButton.setChecked(true);
-                }
-                */
-
-                lightButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        theme=R.style.AppTheme;
-                        newTheme = "light";
-                        Connection.lightOrDark="Light";
-                        lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
-                        darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
-                        followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
-                    }
-                });
-
-                darkButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        theme=R.style.DarkTheme;
-                        newTheme = "dark";
-                        Connection.lightOrDark="Dark";
-                        lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
-                        darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
-                        followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
-                    }
-                });
-
-                followSystemButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        getCurrentSystemTheme();
-                        newTheme = "auto";
-                        Connection.lightOrDark="Follow System";
-                        lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
-                        darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
-                        followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
-                    }
-                });
-
-                cancelTextView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        alertDialog.dismiss();
-                    }
-                });
-
-                applyTextView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        changeTheme(newTheme, alertDialog);
-                    }
-                });
-
-            }
-        });
+        forgottenPasswordSettings = view.findViewById(R.id.forgottenPasswordSettings);
+        forgottenPasswordSettings.setOnClickListener(this);
 
         return view;
     }
 
-    /*
     @Override
     public void onClick(View v) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
         switch (v.getId()) {
-            case R.id.lightRadioButton:
-                newTheme = "light";
+            case R.id.editProfileButton:
+                editProfile(dialogBuilder);
                 break;
-            case R.id.darkRadioButton:
-                newTheme = "dark";
+            case R.id.themeSettings:
+                manageTheme(dialogBuilder);
                 break;
-            case R.id.followSystemRadioButton:
-                newTheme = "auto";
+            case R.id.changePasswordSettings:
+                changePassword(dialogBuilder);
                 break;
-            case R.id.applyTextView:
-                changetheme(newTheme);
+            case R.id.forgottenPasswordSettings:
+                forgottenPassword(dialogBuilder);
                 break;
             default:
                 break;
         }
     }
-    */
 
-    private void getCurrentSystemTheme(){
+
+    private void getCurrentSystemTheme() {
         int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
         switch (nightModeFlags) {
             case Configuration.UI_MODE_NIGHT_NO:
@@ -217,6 +124,181 @@ public class SettingsFragment extends Fragment {
         }
     }
 
+    private void editProfile(AlertDialog.Builder dialogBuilder){
+        //AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
+        dialogBuilder.setView(R.layout.edit_profile_alert_dialog);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        final TextView cancelTextView, applyTextView;
+        cancelTextView = alertDialog.findViewById(R.id.cancelTextView);
+        applyTextView = alertDialog.findViewById(R.id.applyTextView);
+
+        cancelTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        applyTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void changePassword(AlertDialog.Builder dialogBuilder){
+        //AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
+        dialogBuilder.setView(R.layout.change_password_alert_dialog);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        final TextView cancelTextView, confirmTextview;
+        cancelTextView = alertDialog.findViewById(R.id.cancelTextView);
+        confirmTextview = alertDialog.findViewById(R.id.confirmTextView);
+
+        cancelTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        confirmTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void forgottenPassword(AlertDialog.Builder dialogBuilder){
+        //AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
+        dialogBuilder.setView(R.layout.forgotten_password_alert_dialog);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        final TextView cancelTextView, confirmTextview;
+        cancelTextView = alertDialog.findViewById(R.id.cancelTextView);
+        confirmTextview = alertDialog.findViewById(R.id.confirmTextView);
+
+        cancelTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        confirmTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void manageTheme(AlertDialog.Builder dialogBuilder) {
+        //AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
+        dialogBuilder.setView(R.layout.select_theme_alert_dialog);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        final LinearLayout lightButton, darkButton, followSystemButton;
+        final TextView cancelTextView, applyTextView;
+
+        lightButton = alertDialog.findViewById(R.id.lightButton);
+        darkButton = alertDialog.findViewById(R.id.darkButton);
+        followSystemButton = alertDialog.findViewById(R.id.followSystemButton);
+        cancelTextView = alertDialog.findViewById(R.id.cancelTextView);
+        applyTextView = alertDialog.findViewById(R.id.applyTextView);
+
+
+        String currentTheme = sharedPreferences.getString("appTheme", "light");
+        if (currentTheme.equals("dark")) {
+            theme = R.style.DarkTheme;
+            newTheme = "dark";
+            Connection.lightOrDark = "Dark";
+            bgColor = R.color.lightLightBlack;
+            darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
+        } else if (currentTheme.equals("auto")) {
+            getCurrentSystemTheme();
+            newTheme = "auto";
+            Connection.lightOrDark = "Follow System";
+            if (theme == R.style.DarkTheme) {
+                bgColor = R.color.lightLightBlack;
+            } else {
+                bgColor = R.color.mediumWhite;
+            }
+            followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
+        } else {
+            theme = R.style.AppTheme;
+            newTheme = "light";
+            Connection.lightOrDark = "Light";
+            lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
+        }
+                /*
+                if (currentTheme.equals("light")) {
+                    lightButton.setChecked(true);
+                } else if (currentTheme.equals("dark")) {
+                    darkButton.setChecked(true);
+                } else {
+                    followSystemButton.setChecked(true);
+                }
+                */
+
+        lightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                theme = R.style.AppTheme;
+                newTheme = "light";
+                Connection.lightOrDark = "Light";
+                lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
+                darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
+                followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
+            }
+        });
+
+        darkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                theme = R.style.DarkTheme;
+                newTheme = "dark";
+                Connection.lightOrDark = "Dark";
+                lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
+                darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
+                followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
+            }
+        });
+
+        followSystemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentSystemTheme();
+                newTheme = "auto";
+                Connection.lightOrDark = "Follow System";
+                lightButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
+                darkButton.setBackgroundResource(R.drawable.set_current_theme_background_borderless);
+                followSystemButton.setBackgroundResource(R.drawable.set_current_theme_background_borderline);
+            }
+        });
+
+        cancelTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        applyTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTheme(newTheme, alertDialog);
+            }
+        });
+    }
+
     private void changeTheme(String theme, AlertDialog alertDialog) {
 
         sharedPreferences.edit().putString("appTheme", theme).apply();
@@ -226,11 +308,11 @@ public class SettingsFragment extends Fragment {
         Window window = getActivity().getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
-        if(this.theme == R.style.AppTheme){
+        if (this.theme == R.style.AppTheme) {
 
             window.setStatusBarColor(getContext().getColor(R.color.mediumWhite));
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }else{
+        } else {
             window.getDecorView().setSystemUiVisibility(0);
             //window.clearFlags(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
             window.setStatusBarColor(getContext().getColor(R.color.mediumBlack));
