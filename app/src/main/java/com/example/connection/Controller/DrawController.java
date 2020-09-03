1ec209f6da143b2202e17e3a12252aa6c236d6f4
giangiu25307 <gianlucgiardina@libero.it>
2020-09-03 21:26:44 +0200

    MAPPA FUNZIONA

--- app/src/main/java/com/example/connection/Controller/DrawController.java
+++ app/src/main/java/com/example/connection/Controller/DrawController.java
@@ -1,6 +1,5 @@
 package com.example.connection.Controller;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
