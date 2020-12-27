92d2bec73416848cba3b0ae4025624f8520219ed
Luca <lucaber.l9@gmail.com>
2020-12-27 10:50:51 +0100

    Continuata grafica signup

--- app/src/main/java/com/example/connection/View/SignupFragment.java
+++ app/src/main/java/com/example/connection/View/SignupFragment.java
@@ -20,12 +20,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -221,7 +219,7 @@ public class SignupFragment extends Fragment implements View.OnClickListener {
                     @Override
                     public void onClick(View v) {
                         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), R.style.CustomAlertDialog);
-                        dialogBuilder.setView(R.layout.gender_alert_dialog);
+                        dialogBuilder.setView(R.layout.dialog_gender);
                         final AlertDialog alertDialog = dialogBuilder.create();
                         System.out.println("Show non fatto");
                         alertDialog.show();
